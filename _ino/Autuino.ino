0d23ed329014ab0906872c79b0bd4520e6914c4b
Camilo Hernández <Camilohernandezcueto@gmail.com>
2016-11-01 01:16:35 -0300

    Create Autuino.ino

---
+++ _ino/Autuino.ino
@@ -0,0 +1,37 @@
+/* ATENCION!: Esta es la version no apta para Platformio, las
+              librerias incluidas en la carpeta "lib" deben
+              ser manualmente agregadas
+*/
+
+
+
+//***********************Inicio codigo************************//
+
+#include <Arduino.h>
+#include <Servo.h>
+#include <ConMotor.h>
+
+using namespace std;
+
+//*******************Inicio declaraciones*********************//
+
+const int Pin_Motor_A1 = 10;
+const int Pin_Motor_A2 = 9;
+const int Pin_Motor_B1 = 6;
+const int Pin_Motor_B2 = 5;
+
+//*********************Fin declaraciones**********************//
+
+
+//*******************************Setup********************************//
+
+void setup(){
+
+}
+
+void loop(){
+
+  
+}
+
+//************************Fin codigo**************************//
