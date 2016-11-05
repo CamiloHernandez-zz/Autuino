10ba6717efa36c4e4994ae15d8986bd84e70990b
Camilo Hernández <Camilohernandezcueto@gmail.com>
2016-11-05 00:00:10 -0300

    Arreglados probelmas de formato, indentación y agregados comentarios

--- src/main.cpp
+++ src/main.cpp
@@ -6,55 +6,71 @@
 
 using namespace std;
 
-#define trigPin A4 // pins del sensor ultrasonido
-#define echoPin A5
-
 //*******************Inicio declaraciones*********************//
 
+// Pines de los motores
 const int Pin_Motor_A1 = 10;
 const int Pin_Motor_A2 = 9;
 const int Pin_Motor_B1 = 6;
 const int Pin_Motor_B2 = 5;
+
+// Pines del sensor ultrasonido
+const int trigPin = A4;
+const int echoPin = A5;
+
+// Variables del ultrasonido
+    long duracion, distancia;
 
 //*********************Fin declaraciones**********************//
 
 
 //*******************************Setup********************************//
 
+
+// Iniciando la librería
 ConMotor motor(Pin_Motor_A1, Pin_Motor_A2, Pin_Motor_B1, Pin_Motor_B2);
 
 void setup(){
-    Serial.begin(9600); // begin serial communitication
-    Serial.println("Motor test!");
-    pinMode(trigPin, OUTPUT);// set the trig pin to output (Send sound waves)
-    pinMode(echoPin, INPUT);// set the echo pin to input (recieve sound waves)
+
+     // Iniciando serial
+    Serial.begin(9600);
+
     motor.writeMotor('B', 200, true);
     motor.writeMotor('A', 200, true);
+
+    // Declaración sobre E/S de los pines
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+
+    //Mensaje de bienvenida
+    Serial.println("AOK");
 
 }
 
 void loop(){
 
-    long duration, distance; // start the scan
-
     digitalWrite(trigPin, LOW);
-    delayMicroseconds(2); // delays are required for a succesful sensor operation.
+
+    // Se detiene el sensor por 0.002 segundos
+    delayMicroseconds(2);
+
     digitalWrite(trigPin, HIGH);
 
-    delayMicroseconds(10); //this delay is required as well!
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distance = (duration/2) / 29.1;// convert the distance to centimeters.
-    if (distance < 25)/*if there's an obstacle 25 centimers, ahead, do the following: */ {
-     Serial.println ("Close Obstacle detected!" );
-    Serial.println ("Obstacle Details:");
-    Serial.print ("Distance From Robot is " );
-    Serial.print ( distance);
-    Serial.print ( " CM!");// print out the distance in centimeters.
+    // Se detiene el sensor por 0.01 segundos
+    delayMicroseconds(10);
 
-    Serial.println (" The obstacle is declared a threat due to close distance. ");
-    Serial.println (" Turning !");
-}
+    digitalWrite(trigPin, LOW);
+
+    duracion = pulseIn(echoPin, HIGH);
+
+    //Ecuación para calcular la distáncia en centimetros
+    distancia = (duracion/2) / 29.1;
+
+    if (distancia < 25){
+     Serial.print ("Obstaculo detectedo a " );
+     Serial.print (distancia);
+     Serial.println ( " centimetros.");
+    }
 
 }
 
