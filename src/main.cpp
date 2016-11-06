32f09a6d82861ed245fde65653af6f35ff1f9b9e
Camilo Hernández <Camilohernandezcueto@gmail.com>
2016-11-06 13:43:19 -0300

    Cambios mayores de "main.cpp"
    Cambiados los pines para el motor A y el Servo por problemas de comptaibilidad de la librería. Agrupados los comandos del ultrasonido en una nueva función llamada "UltraSonido()" para uso mas eficiente.

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@ using namespace std;
 //*******************Inicio declaraciones*********************//
 
 // Pines de los motores
-const int Pin_Motor_A1 = 10;
-const int Pin_Motor_A2 = 9;
+const int Pin_Motor_A1 = A0;
+const int Pin_Motor_A2 = A1;
 const int Pin_Motor_B1 = 6;
 const int Pin_Motor_B2 = 5;
 
@@ -19,7 +19,7 @@ const int trigPin = A4;
 const int echoPin = A5;
 
 // Variables del ultrasonido
-    long duracion, distancia;
+int duracion, distancia;
 
 //*********************Fin declaraciones**********************//
 
@@ -27,49 +27,72 @@ const int echoPin = A5;
 //*******************************Setup********************************//
 
 
-// Iniciando la librería
+// Iniciando la librería ConMotor
 ConMotor motor(Pin_Motor_A1, Pin_Motor_A2, Pin_Motor_B1, Pin_Motor_B2);
 
+// Iniciando la librería Servo
+Servo servo;
+
+int UltraSonido(){
+
+  digitalWrite(trigPin, LOW);
+
+  // Se detiene el sensor por 0.002 segundos
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+
+  // Se detiene el sensor por 0.01 segundos
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+
+  duracion = pulseIn(echoPin, HIGH);
+
+  //Ecuación para calcular la distancia en centimetros
+  distancia = (duracion/2) / 29.1;
+
+  return distancia;
+}
+
 void setup(){
+
+     //Se agrega el Servo
+     servo.attach(3);
 
      // Iniciando serial
     Serial.begin(9600);
 
-    //motor.writeMotor('B', 225, true);
-    //motor.writeMotor('A', 225, true);
+    motor.writeMotor('B', 225, true);
+    motor.writeMotor('A', 225, true);
 
     // Declaración sobre E/S de los pines
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
+
+    //Moviendo el Servo a la posición inicial
+    servo.write(180);
 
     //Mensaje de bienvenida
     Serial.println("AOK");
 
 }
 
+
+
 void loop(){
 
-    digitalWrite(trigPin, LOW);
-
-    // Se detiene el sensor por 0.002 segundos
-    delayMicroseconds(2);
-
-    digitalWrite(trigPin, HIGH);
-
-    // Se detiene el sensor por 0.01 segundos
-    delayMicroseconds(10);
-
-    digitalWrite(trigPin, LOW);
-
-    duracion = pulseIn(echoPin, HIGH);
-
-    //Ecuación para calcular la distancia en centimetros
-    distancia = (duracion/2) / 29.1;
+    distancia = UltraSonido();
 
     if (distancia < 25){
      Serial.print ("Obstaculo detectedo a " );
      Serial.print (distancia);
      Serial.println ( " centimetros.");
+     Serial.println ( " Revisando a que lado ir.");
+
+     servo.write(180);
+
+
     }
 
 }
