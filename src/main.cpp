27ad8eb9dd4103ec7d2c6a9ebacb326e81909f68
Camilo Hernández <Camilohernandezcueto@gmail.com>
2016-11-07 20:42:30 -0300

    Cambios mayores a "main.cpp"

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,11 @@ using namespace std;
 //*******************Inicio declaraciones*********************//
 
 // Pines de los motores
-const int Pin_Motor_A1 = A0;
-const int Pin_Motor_A2 = A1;
-const int Pin_Motor_B1 = 6;
-const int Pin_Motor_B2 = 5;
+const int Pin_Motor_A1 = 6;
+const int Pin_Motor_A2 = 5;
+const int Pin_Motor_B1 = A0;
+const int Pin_Motor_B2 = A1;
+
 
 // Pines del sensor ultrasonido
 const int trigPin = A4;
@@ -88,48 +89,65 @@ void loop(){
 
     distancia_frontal = UltraSonido();
 
-    if (distancia_frontal < 25){
+    if (distancia_frontal < 30){
+
+    Serial.println ();
+    Serial.println ( "-----------------------------------------------------");
      Serial.print ("Obstaculo detectedo a " );
      Serial.print (distancia_frontal);
      Serial.println ( " centimetros.");
+
      Serial.println ( "Deteniendo motores");
 
      motor.FrenoM();
 
-     Serial.println ( "Revisando a que lado ir.");
+     Serial.println ( "Revisando a que lado voy:");
 
      servo.write(70);
 
      delay(500);
-     distancia_izq = UltraSonido();
+     distancia_der = UltraSonido();
 
      servo.write(180);
 
     delay(500);
-     distancia_der = UltraSonido();
+     distancia_izq = UltraSonido();
 
      delay(500);
      servo.write(125);
 
        if(distancia_der>distancia_izq){
-         motor.writeMotor('A',0,false);
-         motor.writeMotor('B',255,true);
 
-         delay(1000);
+           Serial.print ( "El lado derecho esta mas despejado, girando. (");
+           Serial.print (distancia_der);
+           Serial.println ( " centimetros).");
+
+
+         motor.writeMotor('B',0,false);
+         motor.writeMotor('A',255,false);
+
+         delay(800);
 
          distancia_der = 0;
          distancia_izq = 0;
        }
 
        if(distancia_der<distancia_izq){
-         motor.writeMotor('B',0,true);
-         motor.writeMotor('A',255,false);
 
-         delay(1000);
+         Serial.print ( "El lado izquierdo esta mas despejado, girando. (");
+         Serial.print (distancia_izq);
+         Serial.println ( " centimetros).");
+
+         motor.writeMotor('A',0,true);
+         motor.writeMotor('B',255,false);
+
+         delay(800);
 
          distancia_der = 0;
          distancia_izq = 0;
        }
+
+       Serial.println ( "-----------------------------------------------------");
 
 
     }
